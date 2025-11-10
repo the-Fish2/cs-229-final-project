4bdb31dabc6a39c4fbce7783a9d70f598152ef19
lethan3 <ethanlnow@gmail.com>
2025-11-10 01:19:35 -0800

    guhhhhhhh

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def main():
     samples = function_utils.SAMPLES
 
     X = np.zeros((n_examples, samples))
-    # SE = [None] * n_examples
-    # STR = [None] * n_examples
+    SE = [None] * n_examples
+    STR = [None] * n_examples
     y = np.zeros((n_examples,))
 
     for i in range(n_examples):
@@ -23,22 +23,22 @@ def main():
         complexity = i // (n_examples // 4)
         expr = function_utils.gen_random_example(rng, complexity)
         X[i] = function_utils.gen_values(expr)
-        # SE[i] = expr
-        # STR[i] = function_utils.get_string(expr)
+        SE[i] = expr
+        STR[i] = function_utils.get_string(expr)
         y[i] = complexity
         # print(end='\b'*100)
     print()
 
-    # Save numeric arrays as CSV
-    np.savetxt("X_comp_1_3.csv", X, delimiter=",", fmt="%.10f")
-    np.savetxt("y_comp_1_3.csv", y, delimiter=",", fmt="%.10f")
+    # Save numeric arrays as NPY
+    np.savetxt("X_comp_1_3.npy", X, delimiter=",", fmt="%.10f")
+    np.savetxt("y_comp_1_3.npy", y, delimiter=",", fmt="%.10f")
     
     # Pickle SymPy expressions and strings
-    # with open("SE_comp_1_3.pkl", "wb") as f:
-    #     pickle.dump(SE, f)
+    with open("SE_comp_1_3.pkl", "wb") as f:
+        pickle.dump(SE, f)
     
-    # with open("STR_comp_1_3.pkl", "wb") as f:
-    #     pickle.dump(STR, f)
+    with open("STR_comp_1_3.pkl", "wb") as f:
+        pickle.dump(STR, f)
 
     print("Data saved successfully.")
 
